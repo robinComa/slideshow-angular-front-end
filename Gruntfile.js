ad2d2e65a4e7de5ebfd296134026ea7408b93929
Robin Coma Delperier <rcomadelperier@sqli.com>
2014-01-08 00:59:12 +0100

    Ready for presentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
           '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.app %>/images/{,*/}*.{png,PNG,jpg,jpeg,JPG,JPEG,gif,GIF,webp,svg}'
         ]
       }
     },
